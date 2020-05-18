d90d8b6925708fab17b3ccaed9891ab22b7ffa8a
Naveen0520 <63056210+Naveen0520@users.noreply.github.com>
2020-05-18 11:34:22 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#assignment work:
+
+
+# In[11]:
+
+
+def make_shirt(shirtsize,textmessage):
+    """describing a shirt"""
+    print(f"size of the shirt is {shirtsize}")
+    print(f"{textmessage}: MAKE\n\tYOU\n\tHAPPY")
+
+
+# In[10]:
+
+
+make_shirt('S','for u')
+
+
+# In[12]:
+
+
+make_shirt(textmessage='for u',shirtsize='S')
+
+
+# In[ ]:
+
+
+#LARGE SHIRTS:
+
+
+# In[21]:
+
+
+def large_shirts(size1,size2,size3,txtmsg='I LOVE PYTHON'):
+    """printing message an different shirts"""
+    print(f"shirt size is {size1}\n {txtmsg}\n")
+    print(f"shirt size is {size2}\n {txtmsg}\n")
+    print(f"shirt size is {size3}\n QUEEN")
+
+
+# In[ ]:
+
+
+
+
+
+# In[22]:
+
+
+large_shirts("L","M","S")
+
+
+# In[ ]:
+
+
+#CITIES&COUNTRIES:
+
+
+# In[17]:
+
+
+def describe_city(city='Reykjavik',country='iceland'):
+    """about cities and countries"""
+    print(f"{city}  is in {country}")
+
+
+# In[18]:
+
+
+describe_city()
+
+
+# In[19]:
+
+
+describe_city('Norway','Europe')
+
+
+# In[20]:
+
+
+describe_city('New York','USA')
+
+
+# In[23]:
+
+
+def large_shirts(size,txtmsg='I LOVE PYTHON'):
+    """printing message an different shirts"""
+    print(f"shirt size is {size} \n {txtmsg}")
+
+
+# In[25]:
+
+
+large_shirts('L')
+
+
+# In[26]:
+
+
+large_shirts('M')
+
+
+# In[27]:
+
+
+large_shirts('S','i am the QUEEN')
+
+
+# In[ ]:
+
+
+
+
